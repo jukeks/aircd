b6e3f6579ae24660d6f1574bdd36c5a9bcd38d31
Jukka-Pekka Virtanen <jukka-pekka.virtanen@ssh.com>
2017-04-11 20:56:19 +0300

    Revised perf test

--- src/channeld/tests/performance.go
+++ src/channeld/tests/performance.go
@@ -5,18 +5,33 @@ import (
 	"channeld/protocol"
 	"channeld/server"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
+	"os"
+	"runtime"
 	"strings"
 	"time"
 )
 
 var logger *log.Logger
 
+const NUM_CHANNELS = 50
+
 type Client struct {
 	conn   net.Conn
 	reader *bufio.Reader
+	nick   string
+	id     int
+}
+
+func NewClient(id int, nick string) *Client {
+	c := new(Client)
+	c.id = id
+	c.nick = nick
+
+	return c
 }
 
 func connect() (net.Conn, error) {
@@ -38,7 +53,12 @@ func connect() (net.Conn, error) {
 	return nil, nil
 }
 
-func handshake(nick string) *Client {
+func (c *Client) join(channel string) {
+	protocol.WriteLine(c.conn, fmt.Sprintf("JOIN %s", channel))
+	readUntil(c.reader, fmt.Sprintf("JOIN :%s", channel))
+}
+
+func (c *Client) handshake() {
 	conn, err := connect()
 	if err != nil {
 		panic(err)
@@ -50,17 +70,12 @@ func handshake(nick string) *Client {
 
 	reader := bufio.NewReader(conn)
 
-	client := new(Client)
-	client.conn = conn
-	client.reader = reader
+	c.conn = conn
+	c.reader = reader
 
-	protocol.WriteLine(conn, fmt.Sprintf("NICK %s", nick))
-	protocol.WriteLine(conn, fmt.Sprintf("USER tester localhost localhost :Teppo"))
+	protocol.WriteLine(conn, fmt.Sprintf("NICK %s", c.nick))
+	protocol.WriteLine(conn, fmt.Sprintf("USER %s localhost localhost :Teppo", c.nick))
 	readUntil(reader, "PING")
-	protocol.WriteLine(conn, "JOIN #testers")
-	readUntil(reader, "JOIN :#testers")
-
-	return client
 }
 
 func readUntil(reader *bufio.Reader, until string) {
@@ -77,63 +92,66 @@ func readUntil(reader *bufio.Reader, until string) {
 	}
 }
 
-func readUntilPart(n int, done chan bool) {
-	client := handshake("tester1")
+func readerRoutine(id, numWriters int, done chan int, joined chan bool) {
+	client := NewClient(1, fmt.Sprintf("reader%d", id))
+	client.handshake()
+	client.join(fmt.Sprintf("#testers%d", id))
+	joined <- true
 
-	go func() {
-		messages := 0
-		parts := 0
+	expectedParts := numWriters / NUM_CHANNELS
 
-		defer func() {
-			client.conn.Close()
-			log.Printf("Read %d messages", messages)
-			done <- true
-		}()
+	messages := 0
+	parts := 0
 
-		for {
-			client.conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-			line, err := protocol.ReadLine(client.reader)
-			if err != nil {
-				log.Printf("Bailing: %s", err)
-				return
-			}
-
-			if strings.Contains(line, "PART ") {
-				parts += 1
-				if parts%20 == 0 {
-					log.Printf("PART #%d received", parts)
-				}
-			}
-
-			if parts == n {
-				log.Printf("READ %d PART messages", n)
-				return
-			}
-
-			messages += 1
+	for {
+		client.conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+		line, err := protocol.ReadLine(client.reader)
+		if err != nil {
+			log.Printf("Bailing: %s", err)
+			break
 		}
-	}()
+
+		messages += 1
+
+		if strings.Contains(line, "PART ") {
+			parts += 1
+			if parts%20 == 0 {
+				log.Printf("PART #%d received", parts)
+			}
+		}
+
+		if parts == expectedParts {
+			log.Printf("READ %d PART messages", expectedParts)
+			break
+		}
+	}
+
+	client.conn.Close()
+	log.Printf("Read %d messages", messages)
+	done <- messages
 }
 
-func writeNLines(checkpoint1, checkpoint2, done chan bool, name string, n int) {
-	client := handshake(name)
+func writerRoutine(done chan bool, id int, name string, n int, joined, start chan bool) {
+	client := NewClient(id, name)
+	client.handshake()
 	defer client.conn.Close()
 
-	/*
-		checkpoint1 <- true
-		<-checkpoint2*/
+	channel := fmt.Sprintf("#testers%d", id%NUM_CHANNELS)
+	client.join(channel)
+	joined <- true
+	<-start
 
 	go func() {
 		for i := 0; i < n; i++ {
 			protocol.WriteLine(client.conn,
-				fmt.Sprintf("PRIVMSG tester1 :%s", strings.Repeat("A", 460)))
+				fmt.Sprintf("PRIVMSG %s :%s", channel, strings.Repeat("A", 460)))
 		}
 
-		protocol.WriteLine(client.conn, "PART #testers")
+		protocol.WriteLine(client.conn, fmt.Sprintf("PART %s", channel))
 	}()
 
 	for {
-		client.conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+		client.conn.SetReadDeadline(time.Now().Add(50 * time.Millisecond))
 		line, err := protocol.ReadLine(client.reader)
 		if err != nil {
 			continue
@@ -149,35 +167,58 @@ func writeNLines(checkpoint1, checkpoint2, done chan bool, name string, n int) {
 }
 
 func main() {
+	runtime.GOMAXPROCS(8)
 	s := server.NewServer("test.server.example.org")
 	go s.Serve()
 
-	n := 300
+	numWriters := 1500
+	numMessages := 10
 
-	done := make(chan bool, n+1)
-	checkpoint1 := make(chan bool, n)
-	checkpoint2 := make(chan bool, n)
+	writersDone := make(chan bool, numWriters)
+	messagesRead := make(chan int, NUM_CHANNELS)
+	readersJoined := make(chan bool, NUM_CHANNELS)
+	writersJoined := make(chan bool, numWriters)
+	writersStart := make(chan bool, numWriters)
 
-	readUntilPart(n, done)
+	// joining readers
+	for i := 0; i < NUM_CHANNELS; i++ {
+		go readerRoutine(i, numWriters, messagesRead, readersJoined)
+	}
+	for i := 0; i < NUM_CHANNELS; i++ {
+		<-readersJoined
+	}
 
-	for i := 2; i < n+2; i++ {
+	// joinin gwriters
+	for i := 0; i < numWriters; i++ {
 		func() {
-			go writeNLines(checkpoint1, checkpoint2, done, fmt.Sprintf("tester%d", i), 5)
+			go writerRoutine(writersDone, i, fmt.Sprintf("writer%d", i),
+				numMessages, writersJoined, writersStart)
 		}()
 	}
-
-	/*
-		for i := 0; i < n; i++ {
-			<-checkpoint1
-		}
-		log.Printf("Everyone joined")
-		for i := 0; i < n; i++ {
-			checkpoint2 <- true
-		}*/
-	for i := 0; i < n; i++ {
-		<-done
+	for i := 0; i < numWriters; i++ {
+		<-writersJoined
 	}
 
-	<-done
-	s.Quit()
+	log.SetOutput(ioutil.Discard)
+
+	start := time.Now()
+	for i := 0; i < numWriters; i++ {
+		writersStart <- true
+	}
+
+	for i := 0; i < numWriters; i++ {
+		<-writersDone
+	}
+
+	messages := 0
+	for i := 0; i < NUM_CHANNELS; i++ {
+		messages += <-messagesRead
+	}
+
+	writesDone := time.Since(start)
+
+	log.SetOutput(os.Stdout)
+	log.Printf("Read %d messages in %s", messages, writesDone)
+
+	//s.Quit()
 }
